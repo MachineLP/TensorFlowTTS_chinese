bf4d1d4d263691c2eefbcff3e00a83e684e4e24a
MachineLP <MachineLP@163.com>
2020-09-01 20:43:08 +0800

    添加转pb代码

--- tftts_tfserving/README.md
+++ tftts_tfserving/README.md
@@ -1,17 +1,5 @@
-
 ```
-export PYTHONPATH=${PYTHONPATH}:../tftts_serving_flask
-CUDA_VISIBLE_DEVICES=0 nohup python -u tensorflow_tts_run.py > run.log 2>&1 &
-```
-
-````
-export PYTHONPATH=${PYTHONPATH}:../tftts_serving_flask
-python tests/post.py --text "你好"
-````
-
-
-```
-export PYTHONPATH=${PYTHONPATH}:../tftts_serving_flask
+export PYTHONPATH=${PYTHONPATH}:../tftts_tfserving
 CUDA_VISIBLE_DEVICES=1 python tests/test_ttsmodel.py
 ```
 
--- tftts_tfserving/core/models.py
+++ tftts_tfserving/core/models.py
@@ -20,9 +20,6 @@ from tensorflow_tts.inference import AutoProcessor
 from core.parse_text_add_pause import TTSSegPause
 from core.parse_text_to_pyin import TTSPinYin
 
-from tensorflow_tts.configs import Tacotron2Config
-from tensorflow_tts.models import TFTacotron2
-
 class TTSModel():
 
     def __init__(self):
@@ -30,18 +27,60 @@ class TTSModel():
         tf.config.experimental.set_visible_devices(devices=gpus[0:2], device_type='GPU')
         for gpu in gpus:
             tf.config.experimental.set_memory_growth(gpu, True)
-        self._converter_model()
+        self.__init_model()
+        self.tts_pause = TTSSegPause()
+        self.tts_py = TTSPinYin()
 
-    def _converter_model(self):
-        with open( config.tacotron2_baker ) as f:
-            conf = yaml.load(f, Loader=yaml.Loader)
-        conf = Tacotron2Config(**conf["tacotron2_params"])
-        self.tacotron2 = TFTacotron2(config=conf, training=False, name="tacotron2", enable_tflite_convertible=True)
+    def __init_model(self):
+        tacotron2_config = AutoConfig.from_pretrained( config.tacotron2_baker )
+        self.tacotron2 = TFAutoModel.from_pretrained( config=tacotron2_config, pretrained_path=config.tacotron2_pretrained_path, training=False,  name="tacotron2" )
         self.tacotron2.setup_window(win_front=5, win_back=5)
-        # self.tacotron2.setup_maximum_iterations(1000) # be careful
-        self.tacotron2._build()
-        self.tacotron2.load_weights(config.tacotron2_pretrained_path)
 
-        self.tacotron2.save('./', save_format='tf')
+        tf.saved_model.save(self.tacotron2, "./test_saved", signatures=tacotron2.inference)
+        self.tacotron2 = tf.saved_model.load("./test_saved")
 
+        mb_melgan_config = AutoConfig.from_pretrained( config.multiband_melgan_baker )
+        self.mb_melgan = TFAutoModel.from_pretrained( config=mb_melgan_config, pretrained_path=config.multiband_melgan_pretrained_path, name="mb_melgan" )
+
+        self.processor = AutoProcessor.from_pretrained(pretrained_path=config.baker_mapper_pretrained_path)
+
+
+    def text_to_pinyin_sequence(self, text):
+        # pinyin = self.processor.pinyin_parser(text, style=Style.TONE3, errors="ignore")
+        pinyin, text = self.tts_py.get_pyin(text)
+        new_pinyin = []
+        for x in str(pinyin).split(" "):
+            if "#" not in x:
+                new_pinyin.append(x)
+        phonemes = self.processor.get_phoneme_from_char_and_pinyin(text, new_pinyin)
+        text = " ".join(phonemes)
+        print("phoneme seq: {}".format( text ))
+        logging.info( "[TTSModel] [text_to_pinyin_sequence] phoneme seq:{}".format( text ) )
+        input_ids = self.processor.text_to_sequence(text, inference=False) 
+        return input_ids
+
+
+    def do_synthesis(self, input_text):
+        input_text = self.tts_pause.add_pause(input_text)
+        print ("input_text>>>>", input_text)
+        logging.info( "[TTSModel] [do_synthesis] input_text:{}".format( input_text ) )
+        input_ids = self.processor.text_to_sequence(input_text, inference=True) 
+        
+        _, mel_outputs, stop_token_prediction, alignment_history = self.tacotron2.inference(
+            tf.expand_dims(tf.convert_to_tensor(input_ids, dtype=tf.int32), 0),
+            tf.convert_to_tensor([len(input_ids)], tf.int32),
+            tf.convert_to_tensor([0], dtype=tf.int32) )
+        
+        remove_end = 1024
+        audio = self.mb_melgan.inference(mel_outputs)[0, :-remove_end, 0]
+    
+        return mel_outputs.numpy(), alignment_history.numpy(), audio.numpy() 
+
+
+
+    '''
+    input_text = "如果现在还不了，您可以想办法处理啊，您家人知道您的逾期情况吗？他们能不能帮您周转处理逾期账单？"
+    mels, alignment_history, audios = do_synthesis(input_text)
+    ipd.Audio(audios, rate=24000)
+    '''
 
